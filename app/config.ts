8be835aa218fc6b8a973299324d4809353b60e96
Rahul Malkani <rahulmalkani17@gmail.com>
2024-11-22 02:52:57 +0530

    Website Icon changed

--- app/config.ts
+++ app/config.ts
@@ -2,7 +2,7 @@ export const metaData = {
   baseUrl: "https://rahulmalkani.dev/",
   title: "Rahul Malkani",
   name: "Rahul Malkani",
-  ogImage: "",
+  ogImage: "/preview.webp",
   description:
     "I'm an accomplished Android Application Developer with over 5 years of hands-on experience. I'm skilled in Java & Kotlin programming languages and have a solid grasp of latest Android development concepts including Compose, Flow, Coroutines, Room, Hilt, MVVM, Android SDK, App Architecture, and related technologies. ",
 };
