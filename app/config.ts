1f0444badd5f437021dae38782adbeb2f8e27706
Rahul Malkani <rahulmalkani17@gmail.com>
2024-11-22 11:24:58 +0530

    ogImage linkedin issue fixed

--- app/config.ts
+++ app/config.ts
@@ -4,6 +4,11 @@ export const metaData = {
   name: "Rahul Malkani",
   ogImage: "/banner.jpg",
   description: "I'm an accomplished Android Application Developer with over 5 years of hands-on experience. I'm skilled in Java & Kotlin programming languages and have a solid grasp of latest Android development concepts including Compose, Flow, Coroutines, Room, Hilt, MVVM, Android SDK, App Architecture, and related technologies. ",
+  metaTags: {
+    keywords:"Android Developer, Kotlin, Java, Jetpack Compose, Firebase, MVVM, Android SDK, Mobile App Development",
+    author: "Rahul Malkani",
+    robots: "index, follow", // SEO instructions for search engines
+  },
 };
 
 export const socialLinks = {
