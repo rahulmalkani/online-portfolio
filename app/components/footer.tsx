fb94b853cbf06680422606bfe010e7f066658b87
Rahul Malkani <rahulmalkani17@gmail.com>
2024-11-27 17:38:27 +0530

    SEO fixes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
   return (
     <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
       <time>© {YEAR}</time>{" "}
-        {metaData.title}
+        {metaData.name}
       <style jsx>{`
         @media screen and (max-width: 480px) {
           article {
